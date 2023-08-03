58d198c8e883a8cc3e8d7f6346d7fd8d10c6702b
Yaroslav <yarasity24@gmail.com>
2023-08-03 14:55:04 +0300

    1.0 version

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import { Container } from "components/Container";
+import { Search } from "components/Search";
+import { TheHeader } from "components/TheHeader";
+import { UserCard } from "components/UserCard";
+import { defaultUser } from "mock";
+import { GithubError, GithubUser, LocalGithubUser } from "types";
+import { extractLocalUser } from "utils/extract-local-user";
+import { isGithubUser } from "utils/typeguards";
+
+const BASE_URL = "https://api.github.com/users/";
 
 function App() {
+  const [user, setUser] = useState<LocalGithubUser | null>(defaultUser);
+
+  const fetchUser = async (username: string) => {
+    const url = BASE_URL + username;
+
+    const res = await fetch(url);
+    const user = (await res.json()) as GithubUser | GithubError;
+
+    if (isGithubUser(user)) {
+      setUser(extractLocalUser(user));
+    } else {
+      setUser(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TheHeader />
+      <Search hasError={!user} onSubmit={fetchUser} />
+      {user && <UserCard {...user} />}
+    </Container>
   );
 }
 
---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "wizmine",
+  avatar: "https://avatars.githubusercontent.com/u/108406349?v=4",
+  name: "Yaroslav",
+  company: "",
+  blog: "",
+  location: "Dnipro, Ukraine",
+  bio: "",
+  twitter: "wizmine24",
+  repos: 6,
+  followers: 0,
+  following: 0,
+  created: "2022-06-29T11:57:16Z",
+};
